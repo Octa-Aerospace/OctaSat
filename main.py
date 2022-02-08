01f201f40f6dc620eff11d91a030acd909388e84
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2022-02-07 22:21:30 -0300

    fixing some perfomance errors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from modules.OctaSat import OctaSat
 import os
+import datetime
+from modules.OctaSat import OctaSat
 
 OctaSat = OctaSat()
 
@@ -8,13 +9,16 @@ if __name__ == '__main__':
         try:
             OctaSat.start()
 
-        except OSError:
-            # print("\n[ ! ] Warning: OSError detected, rebooting system.\n")
+        # except OSError:
+        #     # print("\n[ ! ] Warning: OSError detected, rebooting system.\n")
+        #
+        #     os.system("sudo reboot") #* the error should be added to the csv file
 
-            os.system("sudo reboot") #* the error should be added to the csv file
         except KeyboardInterrupt:
             print("\n[ ! ] Exiting\n")
             exit()
 
-        except:
+        except Exception as e:
+            with open("error_log.txt", "a+") as file:
+                file.write("{0} at {1}".format(e, OctaSat.Time()))
             os.system("sudo reboot") # this should be registered too
