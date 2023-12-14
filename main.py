f25fb1c41d5faf6ce2e155527a12f3b3fd9454e2
CxrlosKenobi <yolitopinto@gmail.com>
2023-12-14 17:33:59 -0300

    feat: :sparkles: setting up Buzzer class

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
-import os
-import datetime
-from modules.OctaSat import OctaSat
+#
+from modules.Buzzer import Buzzer
 
-OctaSat = OctaSat()
+PIN_BUZZER = 12
 
-if __name__ == '__main__':
-    while True:
-        try:
-            OctaSat.start()
-
-        # except OSError:
-        #     # print("\n[ ! ] Warning: OSError detected, rebooting system.\n")
-        #
-        #     os.system("sudo reboot") #* the error should be added to the csv file
-
-        except KeyboardInterrupt:
-            print("\n[ ! ] Exiting\n")
-            exit()
-
-        # for unknown errors
-        except Exception as e:
-            with open("/home/pi/OctaSat/data/error_log.txt", "a+") as file:
-                file.write("{0} at {1}\n\n".format(e, OctaSat.Time()))
-            os.system("sudo reboot")
+if __name__ == "__main__":
+  buzzer = Buzzer(PIN_BUZZER)
+  buzzer.play_startup()
+  buzzer.destroy()
